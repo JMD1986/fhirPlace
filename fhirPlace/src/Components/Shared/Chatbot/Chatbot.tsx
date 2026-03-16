4a6422d062efc1201381f9fdd49dad289a86f17a
John Daugherty <Johndaugherty1986@gmail.com>
2026-03-16 14:39:13 -0500

    asdfsdf

--- fhirPlace/src/Components/Shared/Chatbot/Chatbot.tsx
+++ fhirPlace/src/Components/Shared/Chatbot/Chatbot.tsx
@@ -34,6 +34,10 @@ const initialMessages: Message[] = [
 
 
 
+
+
+
+
 export default function Chatbot({ patientContext }: ChatbotProps) {
   const [messages, setMessages] = useState<Message[]>(initialMessages);
   const [input, setInput] = useState("");
